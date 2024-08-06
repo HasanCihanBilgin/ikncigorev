9ae015456887497084b7c06fd1a9644750f6f914
Hascihan26 <hasancihanbilgin@gmail.com>
2024-08-06 21:43:47 +0300

    Initial Commit

---
+++ ikncigorev/ikncigorevApp.swift
@@ -0,0 +1,17 @@
+//
+//  ikncigorevApp.swift
+//  ikncigorev
+//
+//  Created by Hasan Cihan Bilgin on 6.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ikncigorevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
